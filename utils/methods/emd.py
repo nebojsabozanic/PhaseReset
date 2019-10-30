d8cf5c2b11ebd0a94cc7c6803947ed18b880a973
N3bojsa <strance@gmail.com>
2019-10-30 15:12:09 -0700

    time freq p1

--- utils/methods/emd.py
+++ utils/methods/emd.py
@@ -1,7 +1,28 @@
 import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+from PyEMD import EMD
 
 
 def hes(args):
+    # fs = 10e3
+    # N = 1e5
+    # amp = 2 * np.sqrt(2)
+    # noise_power = 0.01 * fs / 2
+    # time = np.arange(N) / float(fs)
+    # mod = 500 * np.cos(2 * np.pi * 0.25 * time)
+    # carrier = amp * np.sin(2 * np.pi * 3e3 * time + mod)
+    # noise = np.random.normal(scale=np.sqrt(noise_power), size = time.shape)
+    # noise *= np.exp(-time / 5)
+    # x = carrier + noise
+    #
+    # f, t, Zxx = signal.stft(x, fs, nperseg=10)
+    # plt.pcolormesh(t, f, np.abs(Zxx), vmin=0, vmax=amp)
+    # plt.title('STFT Magnitude')
+    # plt.ylabel('Frequency [Hz]')
+    # plt.xlabel('Time [sec]')
+    # plt.show()
+
 
     # segment
     for cnt_ch in range(args.singled_out_filtered_notched.shape[0]):
@@ -15,26 +36,52 @@ def hes(args):
 
 
         args.fr_res = 500
-        sta = np.zeros([args.len_uc, 100,  args.fr_res, args.win_l + args.win_r])
+        sta = np.zeros([args.len_uc, 100, args.fr_res, args.fr_res, args.win_l + args.win_r])
 
         for i, uclass in enumerate(args.uc):
             ind = (args.uc_ind == i)
             temp = wind_[ind, :]
             for cnti in range(temp.shape[0]):
-                test = stft(temp[cnti, :])  # calc hist wind_[ind, :]
-                sta[i, cnti, :, :] = test
+                what = temp[cnti, :]
+                f, t, test = signal.stft(temp[cnti, :], args.fs, nperseg=100)  # calc hist wind_[ind, :]
+                if cnti == 0:
+                    stest = test
+                else:
+                    stest += test
+                # f, t, Zxx = signal.stft(x, fs, nperseg=10)
+                # emd = EMD()
+                # IMFs = emd(what)
+
+                # plt.plot(np.abs(signal.hilbert(IMFs[1, :])))
+                # plt.pcolormesh(t, f, np.abs(test))
+                # plt.title('STFT Magnitude')
+                # plt.ylabel('Frequency [Hz]')
+                # plt.xlabel('Time [sec]')
+                # plt.show()
+                # plt.waitforbuttonpress(0.1)
+                # plt.close('all')
+
+            plt.pcolormesh(t, f, np.abs(test))
+            plt.title('STFT Magnitude')
+            plt.ylabel('Frequency [Hz]')
+            plt.xlabel('Time [sec]')
+            plt.show()
+            plt.waitforbuttonpress(0.1)
+            plt.close('all')
+
+                # sta[i, cnti, :, :] = np.abs(test)
 
             # step = np.abs(np.mean(phase_reset_wind[ind, :]))
             # mean_step = np.mean()
 
-            sta_mean[i, :, :] = np.abs(np.mean(sta[i, :, :, :], 0))
-            sta_std[i, :, :] = np.abs(np.std(sta[ind, :, :, :], 0))
+            #sta_mean[i, :, :] = np.abs(np.mean(sta[i, :, :, :], 0))
+            #sta_std[i, :, :] = np.abs(np.std(sta[ind, :, :, :], 0))
 
     #         testimage = np.squeeze(hist_wind[i, :, :])
     #         # fig, ax = plt.subplots(nrows=1, ncols=1)
     #         plt.imshow(testimage)  # , aspect='auto'
     #         plt.title(np.max(testimage))
-    #         #ax.set_adjustable('box-forced')
+    #         # ax.set_adjustable('box-forced')
     #         filename = 'histophases' + str(cnt_ch) + 'ch' + str(i) + 'cl' + '.png'
     #         plt.savefig(os.path.join(args.output_dir, filename), bbox_inches = 'tight', pad_inches = 0)
     #         plt.close()
